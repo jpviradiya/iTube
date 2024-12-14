bf10884a66db9aa43347bbf749e23f00478fe000
jpviradiya <jeelviradiya188@gmail.com>
2024-12-14 08:29:27 +0530

    CH2
    1.change in package.json file(run script)
    2.connection of mongodb atlas
    3.dotenv module setup(index.js,package.json)

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\nMONGOBD CONNECTED SUCCESSFULLY\nDB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (err) {
+    console.error("MONGODB:", err);
+    process.exit(1); // Code: 0 or 1. 0 complete without fail, 1 terminates with failure.
+  }
+};
+export default connectDB;
